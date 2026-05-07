6847ed39b58283c14d899aee9d059fd31397919e
israel1212-hub <israelntwari7@gmail.com>
2026-05-07 07:07:30 -0700

    fix: remove SplashScreen JSX reference from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,6 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <SplashScreen />
           {children}
         </ThemeProvider>
         <TempoInit />
